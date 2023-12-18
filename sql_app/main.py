e3186b60f9a2e6d8332d5264a434aa3c9532377d
DalLidas <126471477+DalLidas@users.noreply.github.com>
2023-12-18 19:11:29 +0300

    Упрощение программы. Отказ от пользователей и авторизации

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 import models
-from schemas import InqusitorBase
+from schemas import DatumBase
 from database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
 
@@ -8,13 +8,27 @@ from fastapi import FastAPI, Body, Request, status, Depends
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi.responses import JSONResponse
-
-from pages.router import router as router_pages
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(router_pages)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
+
+
+@app.get("/base", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
 
 
 @app.exception_handler(RequestValidationError)
@@ -39,28 +53,27 @@ db_dependence = Annotated[Session, Depends(get_db)]
 
 
 # Запрос на создание пользователя
-@app.post("/create_db/")
-async def create_db(inqusitor: InqusitorBase, db: db_dependence):
-    db_obj = models.Inqusitor(inqusitor_id=inqusitor.inqusitor_id, inqusitor_name=inqusitor.inqusitor_name,
-                              inqusitor_pass=inqusitor.inqusitor_pass)
+@app.post("/create_table/")
+async def create_db(datum: DatumBase, db: db_dependence):
+    db_obj = models.Datum(table_id=datum.table_id, table_datum=datum.table_datum)
     db.add(db_obj)
     db.commit()
     db.refresh(db_obj)
 
 
 # Запрос на удаление пользователя
-@app.post("/delete_inqusitor/{inqusitor_id}")
-async def create_db(inqusitor_id: int, db: db_dependence):
-    db_answer = db.query(models.Inqusitor).filter(models.Inqusitor.inqusitor_id == inqusitor_id).first()
+@app.post("/delete_inqusitor/{table_id}")
+async def create_db(table_id: int, db: db_dependence):
+    db_answer = db.query(models.Datum).filter(models.Datum.table_id == table_id).first()
     db.delete(db_answer)
     db.commit()
     return db_answer
 
 
 # Запрос на пользователя по id
-@app.get("/get_inqusitor/{inqusitor_id}")
-async def get_inqusitor(inqusitor_id: int, db: db_dependence):
-    db_answer = db.query(models.Inqusitor).filter(models.Inqusitor.inqusitor_id == inqusitor_id).first()
+@app.get("/get_inqusitor/{table_id}")
+async def get_inqusitor(table_id: int, db: db_dependence):
+    db_answer = db.query(models.Datum).filter(models.Datum.table_id == table_id).first()
     if not db_answer:
         raise HTTPException(status_code=404, detail="User is not exist")
     return db_answer
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,18 +1,8 @@
+from typing import List
 from pydantic import BaseModel, Json
-
-
-class InqusitorBase(BaseModel):
-    inqusitor_id: int
-    inqusitor_name: str
-    inqusitor_pass: str
-
-
-class RelatumBase(BaseModel):
-    table_id: int
-    owner_id: int
-    sort_flag: bool
 
 
 class DatumBase(BaseModel):
     table_id: int
-    table_datum: Json
+    sort_flag: bool
+    table_datum: dict[str, List]
